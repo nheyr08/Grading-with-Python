c34b783e727c0a65c073fd134784f63de6c2f956
Betsaleel Henry <64657102+nheyr08@users.noreply.github.com>
2022-11-29 19:28:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,10 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='gradepy',
-      version='0.3',
-      description='Tools for automated grading of python assignments.',
+setup(name='My Grader',
+      description='Tools for automated grading with python.',
       long_description=readme(),
-      keywords='grade grading python framework student assignment',
-      url='https://github.com/fredcallaway/grade.py',
-      author='Fred Callaway',
-      author_email='fredc@llaway.com',
+      keywords='grade grading student assignment',  
       license='MIT',
       packages=['gradepy'],  
       include_package_data=True,
